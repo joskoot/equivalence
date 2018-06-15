aeffe91d55e1bd414fb0a47cf6dc8bc78c547147
Jos Koot <jos.koot@gmail.com>
2018-06-15 17:07:08 +0200

    Add files via upload

--- eqv-classes.rkt
+++ eqv-classes.rkt
@@ -1,18 +1,24 @@
 #lang racket
 
-; Procedure: (eqv-classes (lst list?) (eq (-> any/c any/c))
-;            -> (listof list?)
-; Every sublist shows an equivalence-class
-; of the elements of lst using equivalence relation eq.
-; eq must be defined for all pairs of elements of the lst,
-; but may be unpredictable for other arguments.
-; eq is not checked to be an equivalence relation for lst.
+(provide eqv-classes equivalence?)
 
-(define (eqv-classes lst eq)
+(define (eqv-classes s eq)
  (define make-immutable-dict
   (let-values (((a b c d e f g) (make-custom-hash-types eq))) e))
  (for/fold
   ((h (make-immutable-dict))
    #:result (dict-values h))
-  ((element (in-list lst)))
-  (dict-set h element (cons element (dict-ref h element '())))))
+  ((element (in-set s)))
+  (dict-set h element (set-add (dict-ref h element (set)) element))))
+
+(define (equivalence? f s)
+ (define set-s (in-set s))
+ (and
+  ;reflexivity
+  (for/and ((x set-s)) (f x x))
+  ;symmetry
+  (for/and ((x set-s) (y set-s))
+   (eq? (and (f x y) #t) (and (f y x) #t)))
+  ;transitivity
+  (for/and ((x set-s) (y set-s) (z set-s) #:when (and (f x y) (f y z)))
+   (f x z))))
